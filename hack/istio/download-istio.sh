7745e0b8bbca7a875ce684848e46e85b747b17f1
mattmahoneyrh <mmahoney@redhat.com>
2020-12-09 14:31:07 -0500

    test

--- hack/istio/download-istio.sh
+++ hack/istio/download-istio.sh
@@ -65,6 +65,12 @@ echo "Output Directory: ${OUTPUT_DIR}"
 if [ -z "${ISTIO_VERSION}" ]; then
   if [ -z "${DEV_ISTIO_VERSION}" ]; then
     # get the latest released version
+    echo "About to curl"
+    curl -L -s https://api.github.com/repos/istio/istio/releases | \
+          grep tag_name | sed "s/ *\"tag_name\": *\"\\(.*\\)\",*/\\1/" | \
+          grep -v -E "(alpha|beta|rc)\.[0-9]$" | sort -t"." -k 1,1 -k 2,2 -k 3,3 -k 4,4 | tail -n 1
+    echo "Done curl"
+
     VERSION_WE_WANT=$(curl -L -s https://api.github.com/repos/istio/istio/releases | \
           grep tag_name | sed "s/ *\"tag_name\": *\"\\(.*\\)\",*/\\1/" | \
           grep -v -E "(alpha|beta|rc)\.[0-9]$" | sort -t"." -k 1,1 -k 2,2 -k 3,3 -k 4,4 | tail -n 1)
